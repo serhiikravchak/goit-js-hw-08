24f95d94bf54abba02da31ca492183f0df3d2183
serhiikravchak <105275364+serhiikravchak@users.noreply.github.com>
2022-10-02 17:27:03 +0200

    bugs fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,16 +11,22 @@ function onInput() {
     const feedbackOutput = {
         email: inputEmail.value,
         message: inputMessage.value,
-    };
+    }
     localStorage.setItem('feedback-form-state', JSON.stringify(feedbackOutput));
 }
 
-if (!localStorage.getItem('feedback-form-state')) {
+if (localStorage.getItem('feedback-form-state')) {
   const storagedData = localStorage.getItem('feedback-form-state');
-  const storagedlDataJSON = JSON.parse(storagedData);
-  inputEmail.value = storagedDataJSON.email;
+    const storagedDataJSON = JSON.parse(storagedData);
+  
+    inputEmail.value = storagedDataJSON.email;
   inputMessage.value = storagedDataJSON.message;
-}
+    }
+    
+
+
+  
+
 
 
 function onSubmit(evt) {
