48cb4d4cbb6d0b201e7c352f6282b92982f06887
serhiikravchak <105275364+serhiikravchak@users.noreply.github.com>
2022-10-02 15:09:48 +0200

    task 2 and 3 added

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from "@vimeo/player";
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+function setTime(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+  console.log(data);
+}
+
+  function onPlay(data) {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (data.seconds !== currentTime) {
+    player
+      .setCurrentTime(currentTime)
+      .catch(function (error) {
+        switch (error.name) {
+          case 'RangeError':
+            break;
+
+          default:
+            break;
+        }
+      });
+  }
+};
+
+player.on('play', onPlay);
+player.on('timeupdate', throttle(setTime, 1000));
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('form');
+const inputEmail = document.querySelector('input');
+const inputMessage = document.querySelector('textarea');
+
+form.addEventListener('submit', onSubmit);
+form.addEventListener('input', throttle(onInput, 500));
+
+function onInput() {
+    const feedbackOutput = {
+        email: inputEmail.value,
+        message: inputMessage.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(feedbackOutput));
+}
+
+if (!localStorage.getItem('feedback-form-state')) {
+  const storagedData = localStorage.getItem('feedback-form-state');
+  const storagedlDataJSON = JSON.parse(storagedData);
+  inputEmail.value = storagedDataJSON.email;
+  inputMessage.value = storagedDataJSON.message;
+}
+
+
+function onSubmit(evt) {
+    evt.preventDefault();
+    const storagedData = localStorage.getItem('feedback-form-state');
+    const storagedlDataJSON = JSON.parse(storagedData);
+    
+    form.reset();
+    console.log(storagedlDataJSON);
+    localStorage.clear();
+}
